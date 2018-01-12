4ca42ba1653765e38629cc3b9a0e493a458ff318
Team7034 <wlhsfrc@gmail.com>
2018-01-11 20:16:55 -0800

    added basic functions

--- main/src/org/usfirst/frc/team7034/robot/Robot.java
+++ main/src/org/usfirst/frc/team7034/robot/Robot.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team7034.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -16,6 +19,10 @@ public class Robot extends IterativeRobot {
 	final String customAuto = "My Auto";
 	String autoSelected;
 	SendableChooser<String> chooser = new SendableChooser<>();
+	
+	RobotDrive robot;
+	Joystick stick;
+	Timer timer;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -26,6 +33,9 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", defaultAuto);
 		chooser.addObject("My Auto", customAuto);
 		SmartDashboard.putData("Auto choices", chooser);
+		robot = new RobotDrive(0, 1);
+		stick = new Joystick(1);
+		timer = new Timer();
 	}
 
 	/**
@@ -68,6 +78,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		
 	}
 
 	/**
