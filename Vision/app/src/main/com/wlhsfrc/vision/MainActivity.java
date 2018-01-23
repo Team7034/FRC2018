5b56cbab37d7d4801e14db92dd0d3531331bde39
Team7034 <wlhsfrc@gmail.com>
2018-01-22 20:23:59 -0800

    worked on setting up app

--- Vision/app/src/main/com/wlhsfrc/vision/MainActivity.java
+++ Vision/app/src/main/com/wlhsfrc/vision/MainActivity.java
@@ -1,10 +1,71 @@
 package com.wlhsfrc.vision;
 
+import com.example.test.R;
+import com.example.test.util.SystemUiHider;
+
 import android.app.Activity;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+
+	/**
+	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after user
+	 * interaction before hiding the system UI.
+	 */
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+	/**
+	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
+	 * will show the system UI visibility upon interaction.
+	 */
+	private static final boolean TOGGLE_ON_CLICK = true;
+
+	/**
+	 * The flags to pass to {@link SystemUiHider#getInstance}.
+	 */
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+	/**
+	 * The instance of the {@link SystemUiHider} for this activity.
+	 */
+	private SystemUiHider mSystemUiHider;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    Log.i(TAG, "called onCreate");
+	    super.onCreate(savedInstanceState);
+	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	    setContentView(R.layout.activity_main);
+	    final View contentView = findViewById(R.id.activity_main);
+	    mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+	    mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+	    mOpenCvCameraView.setCvCameraViewListener(this);
+	}
+	@Override
+	public void onPause()
+	{
+	    super.onPause();
+	    if (mOpenCvCameraView != null)
+	        mOpenCvCameraView.disableView();
+	}
+	public void onDestroy() {
+	    super.onDestroy();
+	    if (mOpenCvCameraView != null)
+	        mOpenCvCameraView.disableView();
+	}
+	public void onCameraViewStarted(int width, int height) {
+	}
+	public void onCameraViewStopped() {
+	}
+	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+	    return inputFrame.rgba();
+	}
 	
-	private CameraBridgeViewBase mOpenCvCameraView;
+private CameraBridgeViewBase mOpenCvCameraView;
 	
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 	    @Override
@@ -28,35 +89,5 @@ public class MainActivity extends Activity {
 	{
 	    super.onResume();
 	    OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
-	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    Log.i(TAG, "called onCreate");
-	    super.onCreate(savedInstanceState);
-	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	    setContentView(R.layout.HelloOpenCvLayout);
-	    mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
-	    mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-	    mOpenCvCameraView.setCvCameraViewListener(this);
-	}
-	@Override
-	public void onPause()
-	{
-	    super.onPause();
-	    if (mOpenCvCameraView != null)
-	        mOpenCvCameraView.disableView();
-	}
-	public void onDestroy() {
-	    super.onDestroy();
-	    if (mOpenCvCameraView != null)
-	        mOpenCvCameraView.disableView();
-	}
-	public void onCameraViewStarted(int width, int height) {
-	}
-	public void onCameraViewStopped() {
-	}
-	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-	    return inputFrame.rgba();
 	}
 }
